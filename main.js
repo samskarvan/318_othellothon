8ac559b0604dfc8aa104ef60565e37a7c15d0567
Alia Wilkinson <aliawilkinson@gmail.com>
2018-04-05 09:47:27 -0700

    safe to ignore - for idea folder that doesn't care that it's in a git ignore file.

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ middleSquares();
 
 var currentPlayer = "black";
 var gameBoard2dArray = [];
+var coinFlipArray = [];
 
 function createGameBoard(columnAmount, rowAmount) {
     var gameBoard = $("#game-area");
@@ -19,7 +20,7 @@ function createGameBoard(columnAmount, rowAmount) {
     for(rowIterate = 0; rowIterate < columnAmount; rowIterate++) {
         var newFirstSquareOfRow = ($("<div>", {
             class: "square front",
-            value: "blank",
+            value: "not-clicked",
             row: rowCountFirst,
             column: 0,
             click: checkForValidEntry
@@ -32,7 +33,7 @@ function createGameBoard(columnAmount, rowAmount) {
         for(colIterate = 0; colIterate < rowAmount-1; colIterate++) {
             var newSquare = ($("<div>", {
                 class: "square front",
-                value: "blank",
+                value: "not-clicked",
                 row: rowCountBuild,
                 column: columnCountBuild,
                 click: checkForValidEntry
@@ -53,29 +54,27 @@ function createPieces() {
     console.log(this);
     $(this).addClass("front", "border-highlight");
 }
-
-var playerWithCurrentTurn="black";
-var testingArray=gameBoard2dArray;
-function flipCoins(gameBoard2Array){
+function flipCoins(gameBoard2Array) {
     //since we don't have ai functionality, user will play for both teams, so default turn will be set to black
     //turn all values in the array to the value of the current turn - call flip function
     //change current player to white after flipping has occured (at end of function)
-    var coinsToBeFlippedIndex=0;
-    while(coinsToBeFlippedIndex<array.length){
+    var coinsToBeFlippedIndex = 0;
+    while (coinsToBeFlippedIndex < array.length) {
         // array[coinsToBeFlippedIndex] flip animation--should divs start with all coin classes at different z indexs and then toggle higher/lower index classes on flip?
         array[coinsToBeFlippedIndex].toggleClass(playerWithCurrentTurn);//class of coin whose turn it is-white or black//);
         coinsToBeFlippedIndex++;
-        if(playerWithCurrentTurn==="black"){
-            playerWithCurrentTurn="white";
-        }else{
-            playerWithCurrentTurn="black";
+        if (playerWithCurrentTurn === "black") {
+            playerWithCurrentTurn = "white";
+        } else {
+            playerWithCurrentTurn = "black";
         }
     }
+}
 
 
 function checkForValidEntry(){
 
-    if ($(this).attr('value') === "blank"){
+    if ($(this).attr('value') === "not-clicked"){
         var valueClicked = $(this).attr('value');
         var rowClicked = parseInt($(this).attr('row'));
         var columnClicked = parseInt($(this).attr('column'));
@@ -89,13 +88,15 @@ function checkForValidEntry(){
         var currentPosition;
         var searchExtender;
 
-        if ( gameBoard2dArray[rowClicked][(columnClicked +1)] === "blank" || gameBoard2dArray[rowClicked][columnClicked+1] === valueClicked){
+        if ( gameBoard2dArray[rowClicked][(columnClicked +1)] === "not-clicked" || gameBoard2dArray[rowClicked][columnClicked+1] === valueClicked){
             console.log('not valid');
         }
         else {
+            // gameBoard2dArray[$(this).attr("row")][$(this).attr("column")] = $(this).attr("value");
+            //push current coordinates into array and the coordinates of the square above
             searchExtender = 2;
             for (currentPosition = columnClicked + 1; currentPosition <= 7; currentPosition++) {
-                if (gameBoard2dArray[rowClicked][columnClicked + searchExtender] === "blank") {
+                if (gameBoard2dArray[rowClicked][columnClicked + searchExtender] === "not-clicked") {
                     return;
                 }
                 if (gameBoard2dArray[rowClicked][columnClicked + searchExtender] === "black") {
@@ -107,13 +108,17 @@ function checkForValidEntry(){
                 }
             }
         }
-        if ( gameBoard2dArray[rowClicked][(columnClicked -1)] === "blank" || gameBoard2dArray[rowClicked][columnClicked-1] === valueClicked){
+        if ( gameBoard2dArray[rowClicked][(columnClicked -1)] === "not-clicked" || gameBoard2dArray[rowClicked][columnClicked-1] === valueClicked){
             console.log('not valid');
         }
         else{
+            // coinFlipArray.push($(this));
+            // console.log(this);
+            // gameBoard2dArray[$(this).attr("row")][$(this).attr("column")] = $(this).attr("value");
+            //push current coordinates into array and the coordinates of the square above
             searchExtender= 2;
             for(currentPosition = columnClicked -1; currentPosition >= 0; currentPosition--) {
-                if (gameBoard2dArray[rowClicked][columnClicked - searchExtender] === "blank") {
+                if (gameBoard2dArray[rowClicked][columnClicked - searchExtender] === "not-clicked") {
                     return;
                 }
                 if (gameBoard2dArray[rowClicked][columnClicked - searchExtender] === "black") {
@@ -131,13 +136,15 @@ function checkForValidEntry(){
         var currentPosition;
         var searchExtender;
 
-        if ( gameBoard2dArray[rowClicked+1][(columnClicked)] === "blank" || gameBoard2dArray[rowClicked+1][columnClicked] === valueClicked){
+        if ( gameBoard2dArray[rowClicked+1][(columnClicked)] === "not-clicked" || gameBoard2dArray[rowClicked+1][columnClicked] === valueClicked){
             console.log('not valid');
         }
         else {
+            // gameBoard2dArray[$(this).attr("row")][$(this).attr("column")] = $(this).attr("value");
+            //push current coordinates into array and the coordinates of the square above
             searchExtender = 2;
             for (currentPosition = rowClicked + 1; currentPosition <= 7; currentPosition++) {
-                if (gameBoard2dArray[rowClicked + searchExtender][columnClicked] === "blank") {
+                if (gameBoard2dArray[rowClicked + searchExtender][columnClicked] === "not-clicked") {
                     return;
                 }
                 if (gameBoard2dArray[rowClicked + searchExtender][columnClicked] === "black") {
@@ -149,13 +156,15 @@ function checkForValidEntry(){
                 }
             }
         }
-        if ( gameBoard2dArray[rowClicked-1][(columnClicked)] === "blank" || gameBoard2dArray[rowClicked-1][columnClicked] === valueClicked){
+        if ( gameBoard2dArray[rowClicked-1][(columnClicked)] === "not-clicked" || gameBoard2dArray[rowClicked-1][columnClicked] === valueClicked){
             console.log('not valid');
         }
         else{
+            // gameBoard2dArray[$(this).attr("row")][$(this).attr("column")] = $(this).attr("value");
+            //push current coordinates into array and the coordinates of the square above
             searchExtender= 2;
             for(currentPosition = columnClicked -1; currentPosition >= 0; currentPosition--){
-                if(gameBoard2dArray[rowClicked-searchExtender][columnClicked] === "blank"){
+                if(gameBoard2dArray[rowClicked-searchExtender][columnClicked] === "not-clicked"){
                     return;
                 }
                 if(gameBoard2dArray[rowClicked-searchExtender][columnClicked] === "black"){
@@ -213,8 +222,8 @@ function resetGame() {
 
 function changeArray() {
     //change Array when dom changes
-    gameBoard2dArray[$(this).attr("row")][$(this).attr("column")] = $(this).attr("value");
-    console.log(this)
+    // gameBoard2dArray[$(this).attr("row")][$(this).attr("column")] = $(this).attr("value");
+    // console.log(this)
 }
 
 //need function to tell which player clicked last
